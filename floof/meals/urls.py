77da98174efb6cf2888852e37e02e9067f0b51d6
ctsag <chrtsago@yahoo.gr>
2025-07-06 14:07:27 +0300

    Adding delete operation for meals and ingredients

--- floof/meals/urls.py
+++ floof/meals/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.meal_index, name='meals'),
-    path('ingredients', views.ingredient_index, name='ingredients')
+    path('delete/<int:id>', views.meal_delete, name='meals_delete'),
+    path('ingredients', views.ingredient_index, name='ingredients'),
+    path('ingredients/delete/<int:id>', views.ingredient_delete, name='ingredients_delete')
 ]
